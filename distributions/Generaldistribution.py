c688346ef98f2838790e5d96196de2d94a64491a
Salma <aljamaSalma@gmail.com>
2022-08-14 18:27:07 +0100

    body of methods compmlete

--- distributions/Generaldistribution.py
+++ distributions/Generaldistribution.py
@@ -9,4 +9,13 @@ class Distribution:
 
 	def read_data_file(self, file_name):
 		""" function to read data from a file format one number per line """
-		
+
+		with open(file_name) ad file: 
+			data_list = []
+			line = file.readline()
+			while line: 
+				data_list.append(int(line))
+				line = file.readline()
+		file.close()
+		self.data = data_list
+	
