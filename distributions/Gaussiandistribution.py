e63debb509d35450e171885cb4cef9a8466f7e9a
Salma <aljamaSalma@gmail.com>
2022-08-14 18:54:08 +0100

    completed plot histogram mathod

--- distributions/Gaussiandistribution.py
+++ distributions/Gaussiandistribution.py
@@ -48,7 +48,36 @@ class Gaussian(Distribution):
 
 	def plot_histogram_pdf(self, n_spaces =50):
 
-		pass
+		mu = self.mean 
+		sigma = self.stdev
+
+		min_range = min(self.data)
+		max_range = max(self.data)
+
+		interval = (max_range  - min_range)/n_spaces
+
+		x = []
+		y = [] 
+
+		for i in range(n_spaces):
+			tmp = min_range + interval*i
+			x.append(tmp)
+			y.append(self.pdf(tmp))
+
+			 fig, axes = plt.subplots(2,sharex=True)
+        	fig.subplots_adjust(hspace=.5)
+	        axes[0].hist(self.data, density=True)
+	        axes[0].set_title('Normed Histogram of Data')
+	        axes[0].set_ylabel('Density')
+
+	        axes[1].plot(x, y)
+	        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+	        axes[0].set_ylabel('Density')
+	        plt.show()
+
+        return x, y
+
+
 
 
 	def __add__(self, other):
